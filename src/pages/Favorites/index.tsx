415f4c419ef2d5c85ead6a28a10cf51e38ed74b1
Albert Dias <albert.uft@gmail.com>
2021-01-22 12:29:57 -0300

    finishing challenge

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -32,7 +32,14 @@ const Favorites: React.FC = () => {
 
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
-      // Load favorite foods from api
+      const response = await api.get('/favorites');
+
+      setFavorites(
+        response.data.map((favorites: Food) => ({
+          ...favorites,
+          formattedPrice: formatValue(favorites.price)
+        }))
+      )
     }
 
     loadFavorites();
